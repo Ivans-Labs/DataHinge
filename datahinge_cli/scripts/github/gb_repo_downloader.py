42b99d69e1b2db7724e0e3f3a6b9ea228a2f910a
Ivan Ryan <ivan@ivanryan.dev>
2024-01-14 07:32:49 -0500

    update: gb downloader module

--- datahinge_cli/scripts/github/gb_repo_downloader.py
+++ datahinge_cli/scripts/github/gb_repo_downloader.py
@@ -33,18 +33,19 @@ def download_repo(repo_url, output_dir, csv_file, headers):
 def main(args_list):
     # Parse command-line arguments
     parser = argparse.ArgumentParser(description="Clone GitHub repositories based on criteria")
-    parser.add_argument("--output-dir", type=str, required=True, help="The directory to save the cloned repositories")
-    parser.add_argument("--stars", type=int, required=False, help="The minimum number of stars for repositories to clone")
-    parser.add_argument("--size-limit", type=int, required=False, help="The maximum size limit in MB for repositories to clone")
-    parser.add_argument("--code-type", type=str, required=False, help="The code type to search for on GitHub")
-    parser.add_argument("--age", type=int, default=8, help="The age of repositories to search for in years")
-    parser.add_argument("--csv-file", type=argparse.FileType('a'), default="cloned_repositories_log.csv", help="The name of the CSV file to log the cloned repositories")
-    parser.add_argument("--by-user", type=str, required=False, help="The username of the repository owner")
-    parser.add_argument("--by-organization", type=str, required=False, help="The organization name to search for repositories")
-    parser.add_argument("--code-percentage", type=int, required=False, help="The percentage of code in a repository to search for")
-    parser.add_argument("--repo-name", type=str, required=False, help="The repository name to search for")
-    parser.add_argument("--contains", type=str, required=False, help="Filter repositories by term contained in the title")
-    parser.add_argument("--github-token", type=str, required=False, help="GitHub personal access token")
+    parser.add_argument("-o", "--output-dir", type=str, required=True, help="The directory to save the cloned repositories")
+    parser.add_argument("-s", "--stars", type=int, required=False, help="The minimum number of stars for repositories to clone")
+    parser.add_argument("-l", "--size-limit", type=int, required=False, help="The maximum size limit in MB for repositories to clone")
+    parser.add_argument("-t", "--code-type", type=str, required=False, help="The code type to search for on GitHub")
+    parser.add_argument("-a", "--age", type=int, default=8, help="The age of repositories to search for in years")
+    parser.add_argument("-c", "--csv-file", type=argparse.FileType('a'), default="cloned_repositories_log.csv", help="The name of the CSV file to log the cloned repositories")
+    parser.add_argument("-u", "--by-user", type=str, required=False, help="The username of the repository owner")
+    parser.add_argument("-org", "--by-organization", type=str, required=False, help="The organization name to search for repositories")
+    parser.add_argument("-p", "--code-percentage", type=int, required=False, help="The percentage of code in a repository to search for")
+    parser.add_argument("-n", "--repo-name", type=str, required=False, help="The repository name to search for")
+    parser.add_argument("-contains", "--contains", type=str, required=False, help="Filter repositories by term contained in the title")
+    parser.add_argument("-tok", "--github-token", type=str, required=False, help="GitHub personal access token")
+    parser.add_argument("-topic", "--by-topic", type=str, required=False, help="The topic to search for on GitHub")
     args = parser.parse_args(args_list)
 
     # Calculate the date range for repository search
@@ -71,6 +72,8 @@ def main(args_list):
         query += f"+size:<={args.code_percentage}%"
     if args.contains:
         query += f"+{args.contains}+in:name"
+    if args.by_topic:
+        query += f"+topic:{args.by_topic}"
 
     per_page = 100
     page = 1
